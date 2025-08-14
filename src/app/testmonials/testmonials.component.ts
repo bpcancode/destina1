6e1c1c47bb0d65d09629fa1f5d7cb63c2d7ae38b
bishal <poudelb891@gmail.com>
2025-08-14 19:56:45 +0545

    ssr removed because we donot need it

--- src/app/testmonials/testmonials.component.ts
+++ src/app/testmonials/testmonials.component.ts
@@ -1,7 +1,6 @@
 import { CommonModule, JsonPipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { error } from 'console';
 import { environment } from '../../environments/environment';
 
 @Component({
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,8 @@
 
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
-// bootstrapApplication(AppComponent, appConfig)
-//   .catch((err) => console.error(err));
 
-
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes), provideHttpClient(withFetch()),]
-});
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
 
